d8f0ff0721fa598929977ce45eecc88f53ff770b
Colin <ledbetter.colin.m@gmail.com>
2019-09-23 13:41:07 -0400

    Added gitignore, added calculatorview, begin ui

--- src/edu/bsu/cs495/binarycalculator/Main.java
+++ src/edu/bsu/cs495/binarycalculator/Main.java
@@ -1,5 +1,12 @@
 package edu.bsu.cs495.binarycalculator;
 
+import edu.bsu.cs495.binarycalculator.model.Calculator;
+import edu.bsu.cs495.binarycalculator.view.CalculatorView;
+
 public class Main {
+
+    public static void main(String args[]) {
+        CalculatorView calculator = new CalculatorView();
+    }
 
 }
--- src/edu/bsu/cs495/binarycalculator/view/CalculatorView.java
+++ src/edu/bsu/cs495/binarycalculator/view/CalculatorView.java
@@ -1,16 +1,47 @@
 package edu.bsu.cs495.binarycalculator.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CalculatorView {
 
     private static final String NAME = "Binary Calculator";
 
     private JFrame jframe;
+    private JPanel displayPanel;
+    private JPanel numberButtonPanel;
+    private JPanel operationButtonPanel;
+    private JTextPane displayTextPane;
+    private JButton zeroButton;
+    private JButton oneButton;
+    private JButton addButton;
+    private JButton subtractButton;
+    private JButton multiplyButton;
+    private JButton divideButton;
+    private JButton equalsButton;
 
     public CalculatorView() {
+        createJFrame();
+        createDisplayPanel();
+        jframe.pack();
+        jframe.setVisible(true);
+    }
+
+    private void createJFrame() {
         jframe = new JFrame();
         jframe.setTitle(NAME);
+        jframe.getContentPane().setLayout(new BoxLayout(jframe.getContentPane(), BoxLayout.PAGE_AXIS));
+
+    }
+
+    private void createDisplayPanel() {
+        displayPanel = new JPanel();
+        displayPanel.setSize(400,100);
+        displayTextPane = new JTextPane();
+        displayPanel.add(displayTextPane);
+        displayTextPane.setSize(400,100);
+        displayTextPane.setMinimumSize(new Dimension(400,100));
+        jframe.add(displayPanel);
     }
 
 }
