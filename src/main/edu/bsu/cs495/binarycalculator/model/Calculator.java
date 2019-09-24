8b8a5f07afea5c669d6d151469fdeaecba346a93
ryland <rdbabusiak@bsu.edu>
2019-09-23 20:23:39 -0400

    Added functionality for Calculator Class . . . add0 and add1 map to 0 & 1 on calculator to allow entry operator classes map to their respective operators
    equals checks that only two values were input, sets the bitstrings to integers, and calls the chosen operation
    
    findResults performs the operation and returns the result as a bitstring

--- src/main/edu/bsu/cs495/binarycalculator/model/Calculator.java
+++ src/main/edu/bsu/cs495/binarycalculator/model/Calculator.java
@@ -1,7 +1,63 @@
 package edu.bsu.cs495.binarycalculator.model;
 
+import java.util.InputMismatchException;
+
 public class Calculator {
+    String binaryString = "";
+    String result;
+    Character operator;
 
+    void add0(){
+        binaryString+="0";
+    }
 
+    void add1(){
+        binaryString+="1";
+    }
 
+    void addition(){
+        binaryString+=',';
+        operator = '+';
+    }
+
+    void subtraction(){
+        binaryString+=',';
+        operator = '-';
+    }
+
+    void multiplication(){
+        binaryString+=',';
+        operator = '*';
+    }
+
+    void division(){
+        binaryString+=',';
+        operator = '/';
+    }
+
+    void equals() {
+        String[] inputStrings = binaryString.split(",",0);
+        if (inputStrings.length != 2){
+            throw new InputMismatchException();
+        }
+        Integer operand1 = Integer.parseInt(inputStrings[0],2);
+        Integer operand2 = Integer.parseInt(inputStrings[1],2);
+
+        result = findResult(operand1, operand2);
+    }
+
+    private String findResult(Integer operand1, Integer operand2){
+        switch (operator){
+            case '+':
+                return Integer.toBinaryString(operand1 + operand2);
+            case '-':
+                return Integer.toBinaryString(operand1 - operand2);
+            case '*':
+                return Integer.toBinaryString(operand1 * operand2);
+            case '/':
+                return Integer.toBinaryString(operand1 / operand2);
+        }
+        return null;
+    }
 }
+
--- src/test/edu/bsu/cs495/binarycalculator/model/CalculatorTest.java
+++ src/test/edu/bsu/cs495/binarycalculator/model/CalculatorTest.java
@@ -1,7 +1,93 @@
 package edu.bsu.cs495.binarycalculator.model;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
 
 class CalculatorTest {
 
+    @Test
+    void add0Test(){
+        Calculator calculator = new Calculator();
+        calculator.binaryString = "10";
+        calculator.add0();
+        assertEquals("100",calculator.binaryString);
+    }
+
+    @Test
+    void add1Test(){
+        Calculator calculator = new Calculator();
+        calculator.binaryString = "10";
+        calculator.add1();
+        assertEquals("101",calculator.binaryString);
+    }
+
+    @Test
+    void additionTest() {
+        Calculator calculator = new Calculator();
+
+        calculator.add1();
+        calculator.add0();
+        calculator.addition();
+        calculator.add1();
+        calculator.equals();
+
+        assertEquals("11",calculator.result);
+
+    }
+
+    @Test
+    void subtractionTest(){
+        Calculator calculator = new Calculator();
+
+        calculator.add1();
+        calculator.add0();
+        calculator.subtraction();
+        calculator.add1();
+        calculator.equals();
+
+        assertEquals("1",calculator.result);
+    }
+
+    @Test
+    void multiplicationTest(){
+        Calculator calculator = new Calculator();
+
+        calculator.add1();
+        calculator.add0();
+        calculator.multiplication();
+        calculator.add1();
+        calculator.add1();
+        calculator.equals();
+
+        assertEquals("110",calculator.result);
+    }
+
+    @Test
+    void divisionTest(){
+        Calculator calculator = new Calculator();
+
+        calculator.add1();
+        calculator.add0();
+        calculator.add0();
+        calculator.division();
+        calculator.add1();
+        calculator.add0();
+        calculator.equals();
+
+        assertEquals("10",calculator.result);
+    }
+
+    @Test
+    void equalsTest(){
+        Calculator calculator = new Calculator();
+
+        calculator.binaryString = "10110,10111";
+        calculator.operator = '+';
+
+        calculator.equals();
+
+        assertEquals("101101",calculator.result);
+    }
 }
